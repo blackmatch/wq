2add2e0c9052c1e09010664bcdaffea59232ad8e
blackmatch <coder.blackmatch@gmail.com>
2022-03-14 23:27:40 +0800

    :bug: Fix getCityCode error in success_jsonpCallback case

--- src/location/index.ts
+++ src/location/index.ts
@@ -1,25 +1,25 @@
 import axios from 'axios';
 
-const baseUrl = "http://toy1.weather.com.cn/search?cityname=";
+const baseUrl = 'http://toy1.weather.com.cn/search?cityname='
 
 const getCityCode = (text: string): string => {
-  const jsonStr = text.replace("(", "").replace(")", "");
-  const arr = JSON.parse(jsonStr);
+  const jsonStr = text.replace('success_jsonpCallback', '').replace('(', '').replace(')', '')
+  const arr = JSON.parse(jsonStr)
   if (!(Array.isArray(arr) && arr.length > 0)) {
     throw new Error('Parse city code failed!')
   }
 
-  return arr[0].ref.split('~')[0];
+  return arr[0].ref.split('~')[0]
 };
 
 export const getCityCodeByName = async (name: string): Promise<string> => {
   const queryName = encodeURI(name.trim());
 
-  const response = await axios.get(`${baseUrl}${queryName}`);
+  const response = await axios.get(`${baseUrl}${queryName}`)
   if (response.status !== 200) {
     throw new Error('Request for city code failed!')
   }
 
-  const htmlText = response.data;
-  return getCityCode(htmlText);
+  const htmlText = response.data
+  return getCityCode(htmlText)
 }
