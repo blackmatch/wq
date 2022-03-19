b4f7aaa81e41ae311e256d54776b44e9aa4f3eb2
blackmatch <coder.blackmatch@gmail.com>
2022-03-20 00:22:33 +0800

    :art: Optimize tests

--- src/weather/__test__/index.test.ts
+++ src/weather/__test__/index.test.ts
@@ -3,7 +3,6 @@ import {getWeatherInfoByCityCode} from '../index'
 describe('getWeatherInfoByCityCode', () => {
   test('success', async () => {
     const weatherInfoList = await getWeatherInfoByCityCode('101280601')
-    console.log(weatherInfoList)
     expect(weatherInfoList.length).toBe(7)
   })
 })
